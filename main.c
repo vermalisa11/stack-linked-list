bba58f5ce0d16dcdf98868bb50f3ce6e4f374595
Murtaza Raja <murt.raja@gmail.com>
2020-09-15 11:18:39 +0530

    adding problems 2 & 3 + cleanup

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "stackconfig.h"
 #include "stack.h"
@@ -9,7 +10,6 @@ void testIntStack() {
     print(&stack);
     push(&stack, 3);
     print(&stack);
-    // /*
     push(&stack, 4);
     print(&stack);
     push(&stack, 5);
@@ -24,7 +24,6 @@ void testCharStack() {
     print(&stack);
     push(&stack, 3 + 'a');
     print(&stack);
-    // /*
     push(&stack, 4 + 'a');
     print(&stack);
     push(&stack, 5 + 'a');
@@ -34,8 +33,66 @@ void testCharStack() {
     printf("%c\n", peek(&stack));
 }
 
+void problemTwoCheckPalindrome() {
+    char *string = "madam";
+    int length = strlen(string);
+
+    Stack leftStack = {NULL};
+    int halfLength = length/2;
+    for(int i=0; i<halfLength; ++i) {
+        push(&leftStack, string[i]);
+    }
+
+    Stack rightStack = {NULL};
+    for(int i=0; i<halfLength; ++i) {
+        push(&rightStack, string[length-i-1]);
+    }
+    int palindrome = 1;
+    while(!isEmpty(&leftStack)) {
+        char left = pop(&leftStack);
+        char right = pop(&rightStack);
+        if(left != right) {
+            palindrome = 0;
+            break;
+        }
+    }
+    if(palindrome) {
+        printf("%s is a palindrome\n", string);
+    } else {
+        printf("%s is not a palindrome\n", string);
+    }
+}
+
+void problemThreeSort() {
+    int MAX = 255;
+    Stack counts[MAX];
+    for(int i=0; i<MAX; ++i) {
+        counts[i].top = NULL;
+    }
+    char *string = "bcfda";
+    int length = strlen(string);
+    for(int i=0; i<length; ++i) {
+        int ch = string[i];
+        push(&counts[ch], ch);
+    }
+    char sorted[length+1];
+    for(int i=0, j=0; i<MAX; ++i) {
+        while(!isEmpty(&counts[i])) {
+            sorted[j++] = pop(&counts[i]);
+        }
+    }
+    sorted[length] = 0;
+    printf("Input: %s\n", string);
+    printf("Output: %s\n", sorted);
+}
+
 int main() {
     testCharStack();
+
+    // refer stackconfig.h to change to an int stack
     // testIntStack();
+    
+    problemTwoCheckPalindrome();
+    problemThreeSort();
     return 0;
 }
--- stackconfig.h
+++ stackconfig.h
@@ -1,1 +1,6 @@
+/*
+    uncomment the line below, to use an int stack
+    comment the line to use a char stack
+*/
+
 // #define INT_STACK
